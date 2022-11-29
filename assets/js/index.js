96aaf88b6237090bb9d8c797b375805f1cc7fce4
cbm <1697195393@qq.com>
2022-11-30 00:19:12 +0800

    完成了个人中心

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,54 +2,6 @@ $(function() {
 
     getUserInfo();
     var layer = layui.layer;
-    // 获取用户的基本信息
-    function getUserInfo() {
-        $.ajax({
-            method: 'GET',
-            url: '/my/userinfo',
-            // headers 就是请求头配置对象
-            // headers: {
-            //     Authorization: localStorage.getItem('token') || ''
-            // },
-            success: function(res) {
-                if (res.status !== 0) {
-                    return layui.layer.msg('获取用户基本信息失败！');
-                }
-                // layui.layer.msg('获取用户基本信息成功！');
-                renderAvatar(res.data);
-            },
-            // 不论成功还是失败，最终都会调用 complete 回调函数
-            // complete: function(res) {
-            //     console.log(res);
-            //     if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
-            //         // 1. 强制清空 token
-            //         localStorage.removeItem('token');
-            //         // 2. 强制跳转到登录页面
-            //         location.href = '/login.html';
-            //     }
-
-            // }
-        });
-    };
-
-    // // 渲染用户的头像
-    function renderAvatar(user) {
-        // 1. 获取用户的名称
-        var name = user.nickname || user.username;
-        // 2. 设置欢迎的文本
-        $('#welcome').html('欢迎&nbsp;&nbsp;' + name);
-        // 3. 按需渲染用户的头像
-        if (user.user_pic !== null) {
-            // 3.1 渲染图片头像
-            $('.layui-nav-img').attr('src', user.user_pic).show();
-            $('.text-avatar').hide();
-        } else {
-            // 3.2 渲染文本头像
-            var first = name.charAt(0).toUpperCase(); // 第一个字母大写
-            $('.layui-nav-img').hide();
-            $('.text-avatar').html(first).show();
-        }
-    }
 
     // 退出
     $('#btnLogout').on('click', function() {
@@ -64,3 +16,52 @@ $(function() {
         });
     });
 });
+
+// 获取用户的基本信息
+function getUserInfo() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        // headers 就是请求头配置对象
+        // headers: {
+        //     Authorization: localStorage.getItem('token') || ''
+        // },
+        success: function(res) {
+            if (res.status !== 0) {
+                return layui.layer.msg('获取用户基本信息失败！');
+            }
+            // layui.layer.msg('获取用户基本信息成功！');
+            renderAvatar(res.data);
+        },
+        // 不论成功还是失败，最终都会调用 complete 回调函数
+        // complete: function(res) {
+        //     console.log(res);
+        //     if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+        //         // 1. 强制清空 token
+        //         localStorage.removeItem('token');
+        //         // 2. 强制跳转到登录页面
+        //         location.href = '/login.html';
+        //     }
+
+        // }
+    });
+};
+
+// // 渲染用户的头像
+function renderAvatar(user) {
+    // 1. 获取用户的名称
+    var name = user.nickname || user.username;
+    // 2. 设置欢迎的文本
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name);
+    // 3. 按需渲染用户的头像
+    if (user.user_pic !== null) {
+        // 3.1 渲染图片头像
+        $('.layui-nav-img').attr('src', user.user_pic).show();
+        $('.text-avatar').hide();
+    } else {
+        // 3.2 渲染文本头像
+        var first = name.charAt(0).toUpperCase(); // 第一个字母大写
+        $('.layui-nav-img').hide();
+        $('.text-avatar').html(first).show();
+    }
+};
